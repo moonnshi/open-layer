be6957cce6a4e7c49686e8bd3183ab484ec6b1de
shoonlaemay <shoonlaemay>
2023-04-11 09:07:23 +0900

    Commit message

---
+++ main.js
@@ -0,0 +1,17 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+  view : new View({
+    center : [0,0],
+    zoom : 1
+  }),
+  layers : [
+    new TileLayer({
+      source : new OSM(),
+    })
+  ],
+  target : 'map'
+});
